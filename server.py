006384512b5bd4402a440679b6b1696c178d5c40
nignig0 <tanitoluwaadebayo@gmail.com>
2024-09-03 17:58:57 +0000

    some thread work

--- server.py
+++ server.py
@@ -1,5 +1,17 @@
 #server code
 import socket
+import threading
+
+def listen_for_connection(socket):
+    conn, addr = socket.accept()
+    print(f'Connection incoming from {addr}')
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    client_socket.connect()
+
+def handle_client(client_socket):
+    while True:
+        data = 
 
 def start_server():
     try:
@@ -8,8 +20,9 @@ def start_server():
         s.listen(2)
         print('Listening for connections')
         
-        conn, addr = s.accept()
-        print(f'Connection from {addr}')
+        thread_one = threading.Thread(target=listen_for_connection, args = (s,), daemon=True)
+        #we create a daemon thread that listens for connections in the background
+
         
         while True:
             data = s.recv(1024)
